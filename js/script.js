52fc52d07e95b6215522a9695290d12812bde51b
chakr0une <soufianbenchakroune7@gmail.com>
2025-03-26 06:54:31 +0000

    Adding Page Router & Active Link

---
+++ js/script.js
@@ -0,0 +1,38 @@
+const global = {
+  currentPage: window.location.pathname,
+};
+
+// Init App
+function init() {
+  switch (global.currentPage) {
+    case '/':
+    case '/index.html':
+      console.log('Home');
+      break;
+    case '/shows.html':
+      console.log('Shows');
+      break;
+    case '/movie-details.html':
+      console.log('Movie Details');
+      break;
+    case '/tv-details.html':
+      console.log('TV Details');
+      break;
+    case '/search.html':
+      console.log('Search');
+      break;
+  }
+  highlightActiveLink();
+}
+
+// Highlight active link
+function highlightActiveLink() {
+  const links = document.querySelectorAll('.nav-link');
+  links.forEach((link) => {
+    if (link.getAttribute('href') === global.currentPage) {
+      link.classList.add('active');
+    }
+  });
+}
+
+document.addEventListener('DOMContentLoaded', init);
